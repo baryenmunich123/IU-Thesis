863c3bb66127946e852f5f21beab8bc41ce16bd1
baryenmunich123 <65822668+baryenmunich123@users.noreply.github.com>
2023-02-03 22:38:17 +0700

    add db

---
+++ database/thesisdb.sql
@@ -0,0 +1,104 @@
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: request_portal
+-- ------------------------------------------------------
+-- Server version	8.0.32
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `account`
+--
+
+DROP TABLE IF EXISTS `account`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `account` (
+  `account_id` int NOT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `type` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`account_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `account`
+--
+
+LOCK TABLES `account` WRITE;
+/*!40000 ALTER TABLE `account` DISABLE KEYS */;
+/*!40000 ALTER TABLE `account` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `data_field`
+--
+
+DROP TABLE IF EXISTS `data_field`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `data_field` (
+  `data_field_no` int NOT NULL AUTO_INCREMENT,
+  `label` varchar(300) NOT NULL,
+  `type` varchar(45) NOT NULL,
+  `is_disabled` tinyint(1) DEFAULT NULL,
+  `form_no` int NOT NULL,
+  PRIMARY KEY (`data_field_no`),
+  KEY `form_no_idx` (`form_no`),
+  CONSTRAINT `form_no` FOREIGN KEY (`form_no`) REFERENCES `form` (`form_no`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `data_field`
+--
+
+LOCK TABLES `data_field` WRITE;
+/*!40000 ALTER TABLE `data_field` DISABLE KEYS */;
+INSERT INTO `data_field` VALUES (1,'Tên sinh viên','text',1,1),(2,'Mã số sinh viên','text',1,1),(3,'Ngày sinh','date',1,1),(4,'Nơi sinh','text',0,1),(5,'Khóa','text',0,1),(6,'Thời gian học tối đa','month',0,1),(7,'Lý do','text',0,1);
+/*!40000 ALTER TABLE `data_field` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `form`
+--
+
+DROP TABLE IF EXISTS `form`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `form` (
+  `form_no` int NOT NULL AUTO_INCREMENT,
+  `form_name` varchar(300) NOT NULL,
+  PRIMARY KEY (`form_no`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `form`
+--
+
+LOCK TABLES `form` WRITE;
+/*!40000 ALTER TABLE `form` DISABLE KEYS */;
+INSERT INTO `form` VALUES (1,'Xác nhận sinh viên'),(2,'Mẫu đơn xin đăng ký nhập học lại'),(3,'Mẫu đơn xin nhập học lại và gia hạn thời gian học'),(4,'Mẫu đơn xin rút khỏi danh sách đình chỉ học tập'),(5,'Mẫu đơn xin tạm ngưng thời gian học'),(6,'Mẫu đơn xin gia hạn luận văn (Thesis dealine extention  request form)'),(7,'Incomplete (I) grade request form'),(8,'Undergraduate thesis registration form');
+/*!40000 ALTER TABLE `form` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-02-03 22:37:50
